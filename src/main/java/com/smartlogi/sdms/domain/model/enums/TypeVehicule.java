c1b6225731e3111e139e633530572f67e87da1fc
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-10-28 14:06:53 +0100

    SDV-19 : creation d'enumeration TypeVehicule

---
+++ src/main/java/com/smartlogi/sdms/domain/model/enums/TypeVehicule.java
@@ -0,0 +1,11 @@
+package com.smartlogi.sdms.domain.model.enums;
+
+/*
+ **
+ * Enum représentant le type de véhicule utilisé par un livreur.
+ */
+public enum TypeVehicule {
+    MOTO,       // Pour petites livraisons urbaines rapides
+    VOITURE,    // Pour colis moyens ou multiples
+    CAMION      // Pour gros colis ou grandes quantités
+}
