a192cd9071af0794f4c6cdd2241de3affe626cd7
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-10-28 14:04:49 +0100

    SDV-17 : creation d'enumeration PriorityColisColis

---
+++ src/main/java/com/smartlogi/sdms/domain/model/enums/PriorityColis.java
@@ -0,0 +1,11 @@
+package com.smartlogi.sdms.domain.model.enums;
+
+
+/**
+ * Enum représentant la priorité d'un colis.
+ */
+public enum PriorityColis {
+    HAUTE,      // Priorité élevée → livraison urgente
+    NORMALE,    // Priorité normale
+    BASSE       // Priorité faible → livraison moins urgente
+}
