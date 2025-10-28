64a1f16def113d21b95a07f20379c5298bb0c995
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-10-28 13:37:46 +0100

    SDV-17 : creation d'enumeration StatusColis

---
+++ src/main/java/com/smartlogi/sdms/domain/model/enums/StatusColis.java
@@ -0,0 +1,9 @@
+package com.smartlogi.sdms.domain.model.enums;
+
+public enum StatusColis {
+    CREE,          // Colis créé par le client expéditeur
+    COLLECTE,      // Colis collecté par le livreur
+    EN_STOCK,      // Colis stocké en entrepôt
+    EN_TRANSIT,    // Colis en cours de livraison
+    LIVRE          // Colis livré au destinataire
+}
