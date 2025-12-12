257389429ef0be2335dccdefb75243fb89b298cc
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-12-12 23:52:03 +0100

    ajouter la classe RefreshToken pour gérer les tokens de rafraîchissement

---
+++ src/main/java/com/smartlogi/sdms/domain/model/entity/RefreshToken.java
@@ -0,0 +1,25 @@
+package com.smartlogi.sdms.domain.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RedisHash(value = "refresh_tokens", timeToLive = 604800)
+public class RefreshToken {
+
+    @Id
+    private String id;
+
+    @Indexed // Hadi bach t-9llbi b username
+    private String username;
+
+}
