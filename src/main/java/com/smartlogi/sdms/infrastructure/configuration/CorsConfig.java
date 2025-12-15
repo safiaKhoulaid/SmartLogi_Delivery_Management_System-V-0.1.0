a589ead722e1a57bb80ac6788e74b2edd46c54cf
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-12-15 11:36:59 +0100

    ajouter la configuration OAuth2 pour l'authentification Google et mettre à jour la configuration CORS

--- src/main/java/com/smartlogi/sdms/infrastructure/configuration/CorsConfig.java
+++ src/main/java/com/smartlogi/sdms/infrastructure/configuration/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200", "https://system-livraison-front.vercel.app/")
+                        .allowedOrigins("http://localhost:4200", "https://system-livraison-front.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
