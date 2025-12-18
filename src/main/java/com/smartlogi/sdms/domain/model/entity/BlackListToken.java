d358c66d4a9e2a3aebc4ac70d42ce7b306689da7
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-12-18 21:15:35 +0100

    ajouter la gestion des tokens blacklistés et mettre à jour la documentation du projet

--- src/main/java/com/smartlogi/sdms/domain/model/entity/BlackListToken.java
+++ src/main/java/com/smartlogi/sdms/domain/model/entity/BlackListToken.java
@@ -1,13 +1,20 @@
 package com.smartlogi.sdms.domain.model.entity;
 
+import org.springframework.data.annotation.Id;
 
-import jakarta.persistence.Id;
-import lombok.Builder;
 import org.springframework.data.redis.core.RedisHash;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @RedisHash(value = "blacklist_tokens", timeToLive = 900)
-@Builder
+@AllArgsConstructor // 👈 ضروري باش تقدري ديري new BlackListToken(token)
+@NoArgsConstructor  // 👈 Redis كيحتاج Constructor خاوي
+@Getter
+@Setter
 public class BlackListToken {
-    @Id
+
+    @Id // دابا هادي ديال org.springframework.data.annotation.Id
     private String accessToken;
 }
